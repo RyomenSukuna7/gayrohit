1fd5415bd3df5baf24e9ad60f1a8ad7a6bdf86c8
RyomenSukuna7 <140910291+RyomenSukuna7@users.noreply.github.com>
2024-09-07 13:53:45 +0530

    Update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();  
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
