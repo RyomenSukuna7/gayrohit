4a294c047c7a34e3bf387feaca035be83fac3c13
RyomenSukuna7 <140910291+RyomenSukuna7@users.noreply.github.com>
2024-09-12 19:33:07 +0530

    Delete public directory

--- public/sideBar.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "225px";
-    document.getElementById("mySidenav").style.zIndex=1;
-    document.getElementById("chat-container").style.zIndex=0;
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
