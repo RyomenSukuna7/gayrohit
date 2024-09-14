7d7e4e3e870c33d0f92ba673ca9996d9571cc47b
RyomenSukuna7 <140910291+RyomenSukuna7@users.noreply.github.com>
2024-09-14 21:13:57 +0530

    Add files via upload

---
+++ public/data.js
@@ -0,0 +1,60 @@
+// import createKindeClient from "https://cdn.jsdelivr.net/npm/@kinde-oss/kinde-auth-pkce-js/+esm";
+
+// (async () => {
+//     const kinde = await createKindeClient({
+//         client_id: "be8f5969eca04865b1d872436d9ef836",
+//         domain: "https://nextunitech.kinde.com",
+//         redirect_uri: "http://localhost:3000"
+//     });
+
+//     const user = kinde.getUser();
+    
+//     if(!(user==undefined)){
+//         const mySidenav=document.getElementById("mySidenav");
+//         const p=document.createElement("p");
+//         const prompt=document.getElementById("prompt").style.display="block";
+//         const button=document.getElementsByClassName("button1");
+//         for(let dis of button){
+//             dis.style.display="block"
+//         }
+//         document.getElementById("login").style.display="none";
+//         document.getElementById("register").style.display="none";
+        
+//         p.textContent+=`user login: ${user.given_name}`;
+//         p.style.cssText+=`text-align:center;font-size:20px;`
+//         mySidenav.insertAdjacentElement("afterbegin",p);
+
+//     }
+    
+        const prompt=document.getElementById("prompt").style.display="none"; 
+        const button=document.getElementsByClassName("button1");
+        for(let dis of button){
+            dis.style.display="none"
+        }
+
+        const a1=document.createElement("a");
+        const a2=document.createElement("a");
+        const form=document.getElementById("chat-form");
+        a1.setAttribute("href","/login");
+        a2.setAttribute("href","/register");
+        a1.textContent="Register";
+        a2.textContent="Sign in";
+
+        a1.style.cssText=`text-decoration:none;color:white;text-align:center;font-size:20px;`;
+        a2.style.cssText=`text-decoration:none;color:white;text-align:center;margin-left:6vw;font-size:20px;`;
+
+        form.style.cssText=`display:flex;justify-content:center;`
+
+        form.appendChild(a1);
+        form.appendChild(a2);
+
+        // a1.addEventListener("click", async () => {
+        //     await kinde.login();
+        // });
+    
+        // a2.addEventListener("click", async () => {
+        //     await kinde.register();
+        // });
+    
+
+// })();
---
+++ public/kinde.js
@@ -0,0 +1,30 @@
+import createKindeClient from "https://cdn.jsdelivr.net/npm/@kinde-oss/kinde-auth-pkce-js/+esm";
+
+(async () => {
+    const kinde = await createKindeClient({
+        client_id: "be8f5969eca04865b1d872436d9ef836",
+        domain: "https://nextunitech.kinde.com",
+        redirect_uri: "http://localhost:3000"
+    });
+
+    document.getElementById("login").addEventListener("click", async () => {
+        await kinde.login();
+    });
+
+    document.getElementById("register").addEventListener("click", async () => {
+        await kinde.register();
+    });
+
+    document.getElementById("logout").addEventListener("click", async () => {
+          await kinde.logout();
+    });
+
+    kinde.onRedirectCallback = (user, appState) => {
+        console.log({ user, appState });
+        if (user) {
+            // Render logged-in view
+        } else {
+            // Render logged-out view
+        }
+    };
+})();
